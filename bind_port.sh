a5ab0837e35ad188b9a5a9410b3a7f4674e6f651
Zhang <leix.n.zhang@intel.com>
2017-12-15 14:32:41 +0800

    add bind function

--- bind_port.sh
+++ bind_port.sh
@@ -1,1 +1,28 @@
 #!/usr/bin/env bash
+
+# Assumes 3 nic guest, with first nic virtio
+DEVICES=$(lspci |egrep "00:0(4|5|6).0 (Eth|RAM)")
+
+## Automatically unbind/rebind PCI devices
+#modprobe igb_uio
+USEDEVICES=""
+for DEVICE in ${DEVICES}; do
+    DEVICE=0000:${DEVICE}
+    UIO_DRIVER=/sys/bus/pci/drivers/igb_uio
+    SYSFS=/sys/bus/pci/devices/${DEVICE}
+
+    if [ ! -d ${SYSFS} ]; then
+        echo "Unable to find device directory: ${SYSFS}"
+        exit 1
+    fi
+
+    # Add the device to the list of supported devices of the UIO driver
+    UEVENT=${SYSFS}/uevent
+    PCI_ID=($(cat ${UEVENT} | grep PCI_ID | sed -e 's/^.*=//' | tr ":" " "))
+    echo "${PCI_ID[0]} ${PCI_ID[1]}" > ${UIO_DRIVER}/new_id
+
+    # Unbind from the old driver and bind to the new driver
+    echo -n ${DEVICE} > ${SYSFS}/driver/unbind
+    echo -n ${DEVICE} > ${UIO_DRIVER}/bind
+    USEDEVICES+=" --use-device ${DEVICE}"
+done
