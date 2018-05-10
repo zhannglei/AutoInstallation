521422d5f665afe17de411986c50bfb82d696a24
leizha9x <1033673266@qq.com>
2018-05-10 13:04:50 +0800

    last

--- install_rpm.sh
+++ install_rpm.sh
@@ -46,42 +46,42 @@ rpm_install -i dstat-0.7.2-12.el7.noarch
 rpm -U vim-common-7.4.160-2.el7.x86_64.rpm vim-enhanced-7.4.160-2.el7.x86_64.rpm vim-filesystem-7.4.160-2.el7.x86_64.rpm gpm-libs-1.20.7-5.el7.x86_64.rpm
 rpm_install -i mlocate-0.26-6.el7.x86_64
 
-#cd ..
-#cd ${qat_rpm_folder}
-#rpm_install -i systemd-devel-219-19.el7_2.13.tis.1.x86_64
-#rpm_install -i libicu-50.1.2-15.el7.x86_64
-#rpm_install -i boost-atomic-1.53.0-27.el7.x86_64
-#rpm_install -i boost-system-1.53.0-27.el7.x86_64
-#rpm_install -i boost-chrono-1.53.0-27.el7.x86_64
-#rpm_install -i boost-context-1.53.0-27.el7.x86_64
-#rpm_install -i boost-date-time-1.53.0-27.el7.x86_64
-#rpm_install -i boost-filesystem-1.53.0-27.el7.x86_64
-#rpm_install -i boost-iostreams-1.53.0-27.el7.x86_64
-#rpm_install -i boost-math-1.53.0-27.el7.x86_64
-#rpm_install -i boost-program-options-1.53.0-27.el7.x86_64
-#rpm_install -i boost-python-1.53.0-27.el7.x86_64
-#rpm_install -i boost-random-1.53.0-27.el7.x86_64
-#rpm_install -i boost-serialization-1.53.0-27.el7.x86_64
-#rpm_install -i boost-signals-1.53.0-27.el7.x86_64
-#rpm_install -i boost-test-1.53.0-27.el7.x86_64
-#rpm_install -i boost-thread-1.53.0-27.el7.x86_64
-#rpm_install -i boost-timer-1.53.0-27.el7.x86_64
-#rpm_install -i boost-wave-1.53.0-27.el7.x86_64
-#rpm_install -i boost-regex-1.53.0-27.el7.x86_64
-#rpm_install -i boost-graph-1.53.0-27.el7.x86_64
-#rpm_install -i boost-locale-1.53.0-27.el7.x86_64
-#
-#rpm_install -i boost-1.53.0-27.el7.x86_64
-#rpm_install -i boost-devel-1.53.0-27.el7.x86_64
-#rpm_install -i zlib-devel-1.2.7-15.el7.x86_64
-#rpm_install -i libsepol-devel-2.1.9-3.el7.x86_64
-#rpm_install -i pcre-devel-8.32-15.el7_2.1.x86_64
-#rpm_install -i libselinux-devel-2.2.2-6.el7.x86_64
-#rpm_install -i keyutils-libs-devel-1.5.8-3.el7.x86_64
-#rpm_install -i libcom_err-devel-1.42.9-7.el7.x86_64
-#rpm_install -i libverto-devel-0.2.5-4.el7.x86_64
-#rpm_install -i krb5-devel-1.13.2-12.el7_2.x86_64
-#rpm_install -i openssl-devel-1.0.1e-51.el7_2.7.x86_64
+cd ..
+cd ${qat_rpm_folder}
+rpm_install -i systemd-devel-219-19.el7_2.13.tis.1.x86_64
+rpm_install -i libicu-50.1.2-15.el7.x86_64
+rpm_install -i boost-atomic-1.53.0-27.el7.x86_64
+rpm_install -i boost-system-1.53.0-27.el7.x86_64
+rpm_install -i boost-chrono-1.53.0-27.el7.x86_64
+rpm_install -i boost-context-1.53.0-27.el7.x86_64
+rpm_install -i boost-date-time-1.53.0-27.el7.x86_64
+rpm_install -i boost-filesystem-1.53.0-27.el7.x86_64
+rpm_install -i boost-iostreams-1.53.0-27.el7.x86_64
+rpm_install -i boost-math-1.53.0-27.el7.x86_64
+rpm_install -i boost-program-options-1.53.0-27.el7.x86_64
+rpm_install -i boost-python-1.53.0-27.el7.x86_64
+rpm_install -i boost-random-1.53.0-27.el7.x86_64
+rpm_install -i boost-serialization-1.53.0-27.el7.x86_64
+rpm_install -i boost-signals-1.53.0-27.el7.x86_64
+rpm_install -i boost-test-1.53.0-27.el7.x86_64
+rpm_install -i boost-thread-1.53.0-27.el7.x86_64
+rpm_install -i boost-timer-1.53.0-27.el7.x86_64
+rpm_install -i boost-wave-1.53.0-27.el7.x86_64
+rpm_install -i boost-regex-1.53.0-27.el7.x86_64
+rpm_install -i boost-graph-1.53.0-27.el7.x86_64
+rpm_install -i boost-locale-1.53.0-27.el7.x86_64
+
+rpm_install -i boost-1.53.0-27.el7.x86_64
+rpm_install -i boost-devel-1.53.0-27.el7.x86_64
+rpm_install -i zlib-devel-1.2.7-15.el7.x86_64
+rpm_install -i libsepol-devel-2.1.9-3.el7.x86_64
+rpm_install -i pcre-devel-8.32-15.el7_2.1.x86_64
+rpm_install -i libselinux-devel-2.2.2-6.el7.x86_64
+rpm_install -i keyutils-libs-devel-1.5.8-3.el7.x86_64
+rpm_install -i libcom_err-devel-1.42.9-7.el7.x86_64
+rpm_install -i libverto-devel-0.2.5-4.el7.x86_64
+rpm_install -i krb5-devel-1.13.2-12.el7_2.x86_64
+rpm_install -i openssl-devel-1.0.1e-51.el7_2.7.x86_64
 
 cd ${SCRIPT_FOLDER}
 . ./install_gtest.sh
